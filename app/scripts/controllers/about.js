39c5e721266f3d25c5832c09d5f58764485b63ce
bianlanzhou <bianlanzhou@gmail.com>
2015-09-15 00:06:22 +0800

    first
    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hmWebappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hmWebappApp
+ */
+angular.module('hmWebappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hmWebappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hmWebappApp
+ */
+angular.module('hmWebappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
